ce08466c2690189b7a376e7ced6e246fa7833d15
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-15 16:16:25 +0100

    for loop added

--- app.js
+++ app.js
@@ -11,16 +11,19 @@ const root = createRoot(container);
 //varibles 
 const title = '';
 const images = [];
+for(const animal in animals) {
+    images.push(<img key= {animal} className="animal" alt={animal} src={animals[animal].image} aria-label={animal} role="button" />)
+};
 const background = <img src="./images/ocean.jpg" className="background" alt="ocean" />;
 const animalFacts = (
 <div>
     <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
     {background}
+    <div className="animals">
+    {images}
+    </div>
 </div>
 );
 
-for(const animal in animals) {
-    images.push(<img key= {animal} className="animal" alt={animal} src={animals[animal.image]} aria-label={animal} role="button" />)
-};
 
 root.render(animalFacts);
