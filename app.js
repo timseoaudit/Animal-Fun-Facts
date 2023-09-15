5a606e50ab0c7968f6982539d10a7b1969de3983
Timneji <139955017+timseoaudit@users.noreply.github.com>
2023-09-15 16:32:12 +0100

    displayFact function

--- app.js
+++ app.js
@@ -10,11 +10,14 @@ const root = createRoot(container);
 //function body
 //varibles 
 const title = '';
+
 const images = [];
-for(const animal in animals) {
+for(const animal in animals) { // for...it loop that will push a img for each data set in the animals.js file 
     images.push(<img key= {animal} className="animal" alt={animal} src={animals[animal].image} aria-label={animal} role="button" />)
-};
+}; // assigns values in JSX 
+
 const background = <img src="./images/ocean.jpg" className="background" alt="ocean" />;
+
 const animalFacts = (
 <div>
     <h1>{title === '' ? 'Click an animal for a fun fact' : title}</h1>
@@ -25,5 +28,10 @@ const animalFacts = (
 </div>
 );
 
+const displayFact = (e) => {
+ const animal = e.target.alt  
+ const Index = Math.floor(Math.random() * animals[animal].facts.length);
+ const funFact = animals[animal].facts[Index];
+};
 
 root.render(animalFacts);
